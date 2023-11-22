971c79a58c4fc2ad981efedf45686c5d90b44393
nafell <pixel68.43@gmail.com>
2023-11-22 21:56:44 +0900

    homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,19 @@
 export default function Home() {
   return (
-    <>Lalafell Masterrace Club</>
+    <>
+      <h1>Lalafell Masterrace Club</h1>
+      <article>
+        <section>
+          <h3>lalafell.fun: </h3>
+          <a href="is.lalafell.fun">bluesky PDS (dev sandbox)</a><br/>
+        </section>
+        <section>
+          <h3>author: </h3>
+          Pathemia<br/>
+          <a href="https://twitter.com/PathemiaLalaM">twitter(X)</a><br/>
+          <a href="https://bsky.app/profile/pathemia.bsky.social">bluesky</a><br/>
+        </section>
+      </article>
+    </>
   )
 }
